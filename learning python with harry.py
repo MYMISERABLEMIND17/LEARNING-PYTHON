e5103c76f4dbb549c54d7fb185659079177f6b9a
Your Name <your.email@example.com>
2025-07-11 16:37:40 +0530

    SECOND FOLDER OF LEARNING PY

---
+++ learning python with harry.py
@@ -0,0 +1,233 @@
+# # CHAPTER 3 
+#                 #  QUESTION 1
+# z=input("name of user : ")                           
+# print("good morinig ",z)
+               # QUESTION 2
+# name = input("name of candidate ")
+# date =input("todays date ")                
+
+# letter =("dear",
+#          name,
+#           "you are selected "
+#          , date)
+# print(letter)
+                
+             #CHAPTER 4 
+# # QUESTION 1 
+# x=input("your list :")
+# y=list(x)
+# print(y)
+
+# # QUESTION 2
+# x=input ("marks of the students :")
+# y=list(x)
+# y.sort()
+# print(y)
+
+# # QUESTION 3
+# w=(2,5,4,3,7,)
+# print(type(w))
+# w.append(9)
+# print(w)
+
+# # QUESTION 5  
+# a=(7,0,8,0,0,9)
+# a.count(0)
+# print(a )
+               #CHAPTER 5
+# # QUESTION 1
+# dict = {"Appetite" :	"Hunger (भूख)",
+#         "Appliance " : " Tools (साधन)",
+#         "Applicable" :	"Fit to be used (काम में लाने योग्य)",
+#         "Application" :	"formal request (अर्जी)",
+#         "Appoint" :"	To engage (काम में लगाना)",
+#         "Appreciate":"To esteem highly (मान देना)",
+#         "Apprehend":"To understand (समझना)",
+#         "Approach" :	"Reach ( पहुँच)" }  
+# x=input("type a word:")    
+# print(dict[x])
+
+# # QUESTION 2
+# x=(input("random 8 numbers : "))
+# y=set(x)
+# print(y)
+
+# # QUESTION 4 
+# s=set()
+# s.add(20)
+# s.add(20.0)
+# s.add("20")
+# print(s)
+# print("len of s:",len(s))
+
+
+# # QUESTION 6
+# p = input("suresh your fav language  :")
+# q = input("mahesh your fav language :")
+# r = input("neelesh your fav language :")
+# s = input("shubhash your fav language: ")
+# dict= {"suresh" :"p" ,
+#        "mahesh": "q",
+#        "neelesh": "r",
+#        "shubhash" :"s"}
+# print(dict)
+
+# # QUESTION 9
+# s={8,7,12,"harry",[1,2]}
+# print(s)
+
+
+               #CHAPTER 6
+# # QUESTION 1 
+# p =  input ("first number :")
+# q =  input  ("second number : ")
+# r = input("third  number : ")
+# s = input ("fourth number :")
+
+# if (p>q):
+#  if(p>r):
+#   if(p>s):
+#     print(p," is the greatest number ")
+# if (q>p):
+#  if(q>r):
+#   if(q>s):
+#      print(q," is the greatest number ")
+# if (r>p):
+#  if(r>q):
+#   if(r>s):
+#     print (r," is the greatest number")
+# if (s>p):
+#  if(s>q):
+#   if(s>r):
+#     print(s," is the greatest number")
+
+# QUESTION 2
+# x=int(input("marks in physics :"))
+# y=int(input("marks in chemistry :"))
+# z=int(input("marks in maths :"))
+# if x>33 :
+#    print("pass in physics ")
+# if y>33 :
+#    print("pass in chemistry ")
+# if z>33 :
+#    print("pass in maths ")
+
+# p=(((x+y+z)/300)*100)
+# print("you got",p)
+# if (p>=40):
+#    print("STATUS = PASS ")
+# else:
+#    print("STATUS = FAIL ") 
+
+
+# # QUESTION 3 
+# x =input("comment something :")
+# if ("Make a lot of money")in x :
+#    print("spam comment detected ")
+# elif ("buy now") in x :
+#     print("spam comment detected ")
+# elif ("subscribe this") in x :
+#      print("spam comment detected ")
+# elif  ("click this") in x :
+#      print("spam comment detected  ")
+# else :
+#    print("no spam comments ")
+
+# QUESTION 4 
+# x=input ("write a user name :")
+# y = len(x)
+# print("no of letters =",y)
+# if y>=10:
+#     print("user name contain >= 10 letters ")
+# if y<=10 :
+#    print ("user name contain <10 letters ")
+
+# # QUESTION 5 
+# list = ["neelesh","mahesh","shubhash","virat","rinku",]
+# x = input ("search :")
+# if (x in list) :
+#    print(x,"is in the list ")
+# else:
+#    print (x,"not in list ")
+
+# # QUESTION 7 
+# x= input("write a paragraph :")
+# if "parakram" in x :
+#     print("this paragraph is talking about parakram ")
+# else:
+#     print ("it is not about parakram")
+    
+
+              #CHAPTER 7
+# # QUESTION 1 
+# x = int(input ("enter a number "))
+# for i in range (0,21,x) :
+#     print(i)
+
+#QUESTION 3 
+# x=int(input("enter  a number :"))
+# i = 1
+# while (i<=10) :
+#     print (i*x)
+#     i = i + 1
+
+# QUESTION 4
+# x = int(input ("enter a number "))
+# for i in range (2,x) :
+#     if (x%i) == 0 :
+#         print (x," is not a prime number ")
+#         break
+#     else :
+#         print (x, "is a prime number")
+
+# # QUESTION 5 
+# x = int(input ("enter a number "))
+# sum =0
+# i=0
+# while (i<=x) :
+#        sum += i
+#        i +=1 
+# print (sum)
+
+# #  QUESTION 6 
+# x = int(input ("enter a number "))
+# product =1 
+# for i in range (1, x+1 ) :
+#     product = product * i 
+# print ("the factorial of x is ", product )
+
+
+                                                               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
